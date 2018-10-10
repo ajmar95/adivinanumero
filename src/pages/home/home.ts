2b76e2d670365eb6118cb6485bfb3927768a9dde
unknown <DAM216@acarballeira.local>
2018-10-10 14:07:38 +0200

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+// tipos primitivos son number , string , boolean , any (cualquier cosa), array
+num : number;
+mayormenor: string="..."; 
+numSecreto : number=this.numAleatorio(0,10) ;
+  constructor(public navCtrl: NavController) {
+
+  }
+  numAleatorio(a,b){
+return Math.round(Math.random()*(b-a)+parseInt(a));
+  }
+compruebaNumero(){
+  if(this.num){
+    if(this.numSecreto < this.num){
+      this.mayormenor= "menor";
+    
+    }else if(this.numSecreto>this.num){
+      this.mayormenor="mayor";
+    }
+    else {
+      this.mayormenor= '';
+    }
+  }
+}
+reinicia(){
+  this.num=null;
+  this.mayormenor="...";
+  this.numSecreto=this.numAleatorio(0,10);
+}
+}
